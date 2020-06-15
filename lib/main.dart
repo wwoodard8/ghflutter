3882fc886d0a6c14f5f5117fe42e27e114706f30
wwoodard8 <will.woodard@scouting.org>
2020-06-15 11:47:36 -0500

    Added local JSON reading, parsing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:async';
-import 'package:path_provider/path_provider.dart';
 
 import 'package:pspdfkit_flutter/pspdfkit.dart';
 
